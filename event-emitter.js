200206e8afa757fe01a075eaa21b6e67b5d8c738
Wael <waelassaf121@gmail.com>
2022-09-15 15:15:33 +0300

    Added Events Emitter File

---
+++ event-emitter.js
@@ -0,0 +1,19 @@
+//Events are a core building block of Node.js
+
+// gets back the class
+// if you want custom extend from class
+// otherwise just for emitting and handling events create instance
+const EventEmitter = require('events')
+
+const customEmitter = new EventEmitter()
+
+customEmitter.on('response', (name, id) => {
+    console.log(`Data received. Name is: ${name} and age is ${id}`)
+})
+
+customEmitter.on('response', () => {
+    console.log(`Some other logic here..`)
+})
+
+customEmitter.emit('response', 'Wael', '26')
+
