aeacb28c3148bfb1b83e55ad938b2bb42abc1d51
Wael <waelassaf121@gmail.com>
2022-09-16 23:09:17 +0300

    First Push

--- 19-http-stream.js
+++ 19-http-stream.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 //Notice: Transfer-Encoding: chunked
 http
     .createServer((req, res) => {
-        // const text = fs.readFileSync('./content/big.txt', 'utf8')
+        // const text = fs.readFileSync('./content/bigger.txt', 'utf8')
         // res.end(text)
         const fileStream = fs.createReadStream('./content/bigger.txt', 'utf8')
         fileStream.on('open', () => {
